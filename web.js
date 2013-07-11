d9bba6e95f4443a47989f65e5388faf027914378
Rich Bayliss <richard@thescarydoor.com>
2013-07-11 01:25:12 +0000

    update to web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   //response.send('Hello World 2!');
-  response.send(buf.toString(fs.readFileSync(index.html));
+  response.send(fs.readFileSync(index.html,buf.toString());
 });
 
 var port = process.env.PORT || 5000;
